80bb4524fc01a9fa7df30672d418b3a75fe17c0a
superHeroLD <superhero_L@163.com>
2021-03-23 20:45:02 +0800

    1.[A] Complete the project construction

---
+++ server-order/src/main/java/cn/ld/peach/mall/order/ServerOrderApplication.java
@@ -0,0 +1,15 @@
+package cn.ld.peach.mall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lidong
+ * @date 2021/3/23
+ */
+@SpringBootApplication
+public class ServerOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerOrderApplication.class, args);
+    }
+}
