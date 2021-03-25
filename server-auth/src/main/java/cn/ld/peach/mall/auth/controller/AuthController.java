3d3998e7a4ad0b5d72539555af9c5c79ce34c535
superHeroLD <superhero_L@163.com>
2021-03-25 22:02:20 +0800

    1.[A] Complete part of the code skeleton

--- server-auth/src/main/java/cn/ld/peach/mall/auth/controller/AuthController.java
+++ server-auth/src/main/java/cn/ld/peach/mall/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package cn.ld.peach.mall.auth.controller;
 import cn.ld.peach.mall.auth.domain.vo.AuthPriorityVO;
 import cn.ld.peach.mall.auth.service.AuthService;
 import cn.ld.peach.mall.commons.util.BeanCopierUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.List;
  * @author lidong
  * @date 2021/3/25
  */
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
