c586db0c937d97c52c603d887437d303f89188c3
dong.li <dong.li@funplus.biz>
2021-03-27 15:23:07 +0800

    1.[A] 调整一下版本依赖

--- server-auth/src/main/java/cn/ld/peach/mall/auth/controller/AuthController.java
+++ server-auth/src/main/java/cn/ld/peach/mall/auth/controller/AuthController.java
@@ -5,6 +5,8 @@ import cn.ld.peach.mall.auth.domain.vo.AuthPriorityVO;
 import cn.ld.peach.mall.auth.service.AuthService;
 import cn.ld.peach.mall.commons.util.BeanCopierUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,7 @@ import java.util.List;
  * @date 2021/3/25
  */
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -28,13 +31,16 @@ public class AuthController {
     @Resource
     private NacosConfig nacosConfig;
 
+    @Value("${message:nulla}")
+    private String message;
+
     @GetMapping("/listRootAuthPriority")
     List<AuthPriorityVO> listRootAuthPriority() {
         return BeanCopierUtil.copyList(authService.listRootAuthPriority(), AuthPriorityVO.class);
     }
 
-    @GetMapping("/getAppName")
-    String getAppName() {
+    @GetMapping("/testNacosConfig")
+    String testNacosConfig() {
         String appName = nacosConfig.getAppName();
         log.info("appName: {}", appName);
         return appName;
